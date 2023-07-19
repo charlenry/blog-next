3448dd63c411d69d7a8615f4bcc9607dde115dc1
Charles-Henri St-Mars <ch.stmars@gmail.com>
2023-07-19 20:47:00 +0200

    first commit

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -2,5 +2,5 @@ import fs from "fs";
 import path from "path";
 
 export async function GET(request) {
-  return new Response("Hello World")
+  return new Response("Hello, this is Charles-Henri's API test")
 }
